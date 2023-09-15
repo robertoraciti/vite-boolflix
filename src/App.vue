1c894f57610c7beafa2d75764a02d425a3c75f85
robertoraciti <rob.raciti97@gmail.com>
2023-09-15 10:20:37 +0200

    axios config

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,37 @@
 <script>
+import axios from "axios";
 import AppHeader from "./components/AppHeader.vue";
-import AppMain from "./components/AppMain.vue";
+import AppMain from "./components/AppMain";
 
 export default {
+  data() {
+    return {
+      movies: [],
+    };
+  },
+
   components: {
     AppHeader,
     AppMain,
+  },
+
+  methods: {
+    fetchMovies() {
+      axios
+        .get("https://api.themoviedb.org/3/search/movie", {
+          params: {
+            query: "anelli",
+            api_key: "321fee9b963d1057eaa31792bfd9c2e9",
+          },
+        })
+        .then((response) => {
+          this.movies = response.data.results;
+        });
+    },
+  },
+
+  created() {
+    fetchMovies();
   },
 };
 </script>
